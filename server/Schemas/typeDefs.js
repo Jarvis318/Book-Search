f3f9699a874abc6a15776f419d10afc1117a6d51
Jarvis <jarvwashington@gmail.com>
2024-09-13 15:11:22 -0500

    updated resolvers and typeDefs

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,6 +1,55 @@
 // Query` type:
+//Make sure they are capitalized. Where i see require, I will use (!)
+//For mongoDB, id is normally always required
+// This .js file is for FETCHING data from the database.
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+    bookCount:Int
+  }
 
-//       * `me`: Which returns a `User` type.
+  type Book {
+  description: String!
+  bookId: ID!
+  image: String
+  link: String
+  title: String!
+  authors:[String]
+  }
+
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookData{
+  description: String!
+  bookId: String!
+  image: String
+  link: String
+  title: String!
+  authors:[String]
+  }
+
+  type Query {
+    me: User
+  }
+
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookInput: BookData) : User
+    deleteBook(bookId: ID!) : User 
+  }
+`;
+
+
+
   
 //     * `Mutation` type:
 
@@ -43,3 +92,4 @@
 //       * `token`
 
 //       * `user` (References the `User` type.)
+module.exports = typeDefs;
